0f71f74c4f2b6a66098cb44cf5dde5a45a46e202
tuutaii <86148583+tuutaii@users.noreply.github.com>
2021-07-23 16:48:15 +0700

    update screen1

--- lib/routes/Walkthrough/screen1.dart
+++ lib/routes/Walkthrough/screen1.dart
@@ -11,7 +11,7 @@ class Screen1 extends StatelessWidget {
             
             children: <Widget>[
                Image.asset('assets/images/fill1.png'),
-               Text('aking', style: TextStyle(color: Colors.red)) 
+               Text('aking', style: TextStyle(color: Colors.blue)) 
             ] 
              
           ),
