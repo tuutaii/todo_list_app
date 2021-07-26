ee6b91d4ecddeba65281ebdefd1d6c3ad296058f
tuutaii <86148583+tuutaii@users.noreply.github.com>
2021-07-26 16:54:55 +0700

    subb

--- lib/routes/Sign_In/forgetpass.dart
+++ lib/routes/Sign_In/forgetpass.dart
@@ -7,6 +7,7 @@ class Forget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
 
       body: SafeArea(
         
